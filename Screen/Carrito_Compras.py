468b58a5fb108a3511ecad85c3c721ba5476d429
Josmar360 <josmargustavopalominocastelan@gmail.com>
2024-06-30 02:12:30 -0600

    Paso de parametros de pantallas anteriores

--- Screen/Carrito_Compras.py
+++ Screen/Carrito_Compras.py
@@ -32,7 +32,7 @@ class Carrito_Compras(Screen):
         global total_a_pagar
         total_a_pagar = 0.0
 
-        # Recorrer selected_products para mostrar cada producto
+        # Recorrer selected_products para mostrar cada producto en la pantalla
         for product in selected_products.values():
             if product['quantity'] > 0:
                 subtotal = product['quantity'] * product['price']
@@ -56,7 +56,7 @@ class Carrito_Compras(Screen):
 
         scrollview.add_widget(layout)
 
-        # Etiqueta de Total
+        # Etiqueta de Total a pagar
         total_label = Label(text=f"Total a Pagar: ${
                             total_a_pagar:.2f}", font_size='24sp', size_hint_y=None, height=50, halign='center', valign='middle')
         total_label.bind(size=total_label.setter(
@@ -69,27 +69,30 @@ class Carrito_Compras(Screen):
         self.add_widget(scrollview)
         self.add_widget(bottom_layout)
 
-        # Agregar los botones de Salir y Pagar
+        # Agregar los botones de Regresar y Pagar
         button_layout = GridLayout(cols=2, size_hint=(1, None), height=50)
-        pagar_button = Button(text='Pagar', size_hint=(0.5, 1))
-        pagar_button.bind(on_press=self.go_to_pagar)
         regresar_button = Button(text='Regresar', size_hint=(0.5, 1))
         regresar_button.bind(on_press=self.go_to_menu_alimentos)
+        pagar_button = Button(text='Pagar', size_hint=(0.5, 1))
+        pagar_button.bind(on_press=self.go_to_pagar)
 
         button_layout.add_widget(regresar_button)
         button_layout.add_widget(pagar_button)
 
         self.add_widget(button_layout)
 
+    # Función para ir a la pantalla de pago y actualizar el precio total en el pedido realizado 
     def go_to_pagar(self, instance):
         self.manager.current = 'realizar_pedido'
         self.manager.get_screen('realizar_pedido').actualizar_precio_total(total_a_pagar)
 
 
         self.manager.current = 'pagar'
-
+    
+    # Función para regresar el menu de alimentos
     def go_to_menu_alimentos(self, instance):
         self.manager.current = 'menu_alimentos'
 
+    # Actualizar la pantalla
     def on_pre_enter(self):
         self.update_screen()
