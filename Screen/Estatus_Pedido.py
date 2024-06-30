6f5968c2ab6f168c01293d262a560228dc408f35
Josmar360 <josmargustavopalominocastelan@gmail.com>
2024-06-30 02:48:46 -0600

    Configuración de conexion a base de datos

--- Screen/Estatus_Pedido.py
+++ Screen/Estatus_Pedido.py
@@ -9,7 +9,15 @@ from kivy.uix.scrollview import ScrollView
 from kivy.uix.popup import Popup
 import mysql.connector
 
+# Configuración de la base de datos
+configuracion = {
+    'host': 'localhost', # Configurar con la IP de tu host
+    'user': 'root', # Configurar tu usuario que esta en red
+    'password': 'Sarinha_3', # Colocar la contraseña de tu usuario
+    'database': 'Dynamics_Dine' # No modificar este parametro
+}
 
+# Clase para colocar las tarjetas de los productos
 class ProductCard(BoxLayout):
     def __init__(self, product_name, product_image, product_status, **kwargs):
         super().__init__(**kwargs)
@@ -38,16 +46,17 @@ class ProductCard(BoxLayout):
         self.add_widget(status_label)
 
 
+# Clase principal para el estatus del pedido
 class Estatus_Pedido(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.mesa_seleccionada = None  # Inicializa la variable para el número de mesa
+        self.mesa_seleccionada = None  # Inicializa la variable para el número de mesa en None
 
         # Layout principal
         layout = BoxLayout(orientation='vertical', padding=[
                            30, 20, 30, 30], spacing=10)  # Ajuste de spacing aquí
 
-        # Título centrado
+        # Título centrado y grande
         titulo = Label(text='Estatus de pedido', halign='center',
                        valign='middle', size_hint=(1, None), height=60, font_size=50)
         layout.add_widget(titulo)
@@ -78,13 +87,16 @@ class Estatus_Pedido(Screen):
         # Ajuste de height aquí
         layout.add_widget(BoxLayout(size_hint_y=None, height=0))
 
-        # Agregar los botones de Salir y Pagar
+        # Agregar los botones de Menu de alimentos, Actualizar y Recoger
         button_layout = GridLayout(cols=3, size_hint=(1, None), height=50)
+        # Boton para ir al menu de alimentos
         menu_alimentos_button = Button(
             text='Menu Alimentos', size_hint=(0.5, 1))
         menu_alimentos_button.bind(on_press=self.go_to_menu_alimentos)
+        # Boton para actualizar el estatus del pedido
         actualizar_button = Button(text='Actualizar', size_hint=(0.5, 1))
         actualizar_button.bind(on_press=self.go_to_actualizar)
+        # Boton para recoger el pedido
         recoger_button = Button(text='Recoger', size_hint=(0.5, 1))
         recoger_button.bind(on_press=self.go_to_recoger)
 
@@ -96,24 +108,21 @@ class Estatus_Pedido(Screen):
 
         self.add_widget(layout)
 
+    # Funcion para actualizar el numero de la mesa
     def update_selected_table(self, mesa_seleccionada):
         self.mesa_seleccionada = mesa_seleccionada
 
         # Actualizar la pantalla con la nueva mesa seleccionada
         self.consultar_estatus_pedidos()
 
+    # Funcion para consultar el estatus de los productos
     def consultar_estatus_pedidos(self):
         try:
             if self.mesa_seleccionada is None:
                 return  # Si no hay mesa seleccionada, no hacer la consulta
 
             # Conexión a la base de datos
-            conexion = mysql.connector.connect(
-                host='localhost',
-                user='root',
-                password='Sarinha_3',
-                database='Dynamics_Dine'
-            )
+            conexion = mysql.connector.connect(**configuracion)
 
             cursor = conexion.cursor()
 
@@ -137,7 +146,7 @@ class Estatus_Pedido(Screen):
                     pk_num_pedido}'
                 self.numero_mesa_label.text = f'Número de mesa: {mesa}'
 
-                # Crear la tarjeta de producto
+                # Crear la tarjeta de producto en proceso de preparacion
                 card = ProductCard(product_name=platillo, product_image=f'Image/{fk_platillo}.jpg',
                                    product_status=self.get_status_text(estatus))
                 self.estatus_layout.add_widget(card)
@@ -149,6 +158,7 @@ class Estatus_Pedido(Screen):
             cursor.close()
             conexion.close()
 
+    # Funcion para ver el estatus del producto
     def get_status_text(self, estatus):
         if estatus == 0:
             return "Su comida se está preparando"
@@ -157,24 +167,22 @@ class Estatus_Pedido(Screen):
         else:
             return f"Estatus: {estatus}"
 
+    # Funcion para cambiar de pantalla a menu de alimentos
     def go_to_menu_alimentos(self, instance):
         self.manager.current = 'menu_alimentos'
 
+    # Funcion para actualizar la pantalla de estatus
     def go_to_actualizar(self, instance):
         self.consultar_estatus_pedidos()
 
+    # Funcion para actualizar la base de datos de productos recogidos
     def go_to_recoger(self, instance):
         try:
             if self.mesa_seleccionada is None:
                 return  # No hacer nada si no hay mesa seleccionada
 
             # Conexión a la base de datos
-            conexion = mysql.connector.connect(
-                host='localhost',
-                user='root',
-                password='Sarinha_3',
-                database='Dynamics_Dine'
-            )
+            conexion = mysql.connector.connect(**configuracion)
 
             cursor = conexion.cursor()
 
@@ -189,7 +197,7 @@ class Estatus_Pedido(Screen):
             cursor.execute(update_query, (self.mesa_seleccionada,))
             conexion.commit()
 
-            # Mostrar mensaje de éxito
+            # Mostrar mensaje de éxito  en notificacion
             popup = Popup(title='Éxito',
                           content=Label(
                               text='Productos recogidos correctamente.'),
@@ -201,7 +209,7 @@ class Estatus_Pedido(Screen):
 
         except mysql.connector.Error as err:
             print(f"Error: {err}")
-            # Mostrar mensaje de error
+            # Mostrar mensaje de error si no se pudo insertar los datos en SQL
             popup = Popup(title='Error',
                           content=Label(
                               text=f'Error al recoger productos: {err}'),
